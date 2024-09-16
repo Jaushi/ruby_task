cbd49ffedf622464fb70fc7bc63427d9aea5cbe5
Jaushi <joshpagallaman10@gmail.com>
2024-09-16 14:29:35 +0800

    Modified: Added a search feature and displays the last 5 Persons

--- main.rb
+++ main.rb
@@ -1,4 +1,15 @@
-personal_data = [
+def clear_terminal
+  system("clear") || system("cls")
+end
+
+def display_citizen_data(citizen_data)
+  sorted_data = citizen_data.sort_by { |person| -person[:created_at].to_i }
+  sorted_data.first(5).each_with_index do |person, index|
+    puts "Person #{index + 1}: #{person[:national_id]} #{person[:name]}, #{person[:age]}"
+  end
+end
+
+citizen_data = [
   { national_id: "1234-5678-9012", name: "Santos", age: 19, created_at: Time.now },
   { national_id: "3456-7890-1234", name: "Reyes", age: 18, created_at: Time.now },
   { national_id: "5678-9012-3456", name: "Cruz", age: 21, created_at: Time.now },
@@ -21,72 +32,80 @@ personal_data = [
   { national_id: "4264-4451-5545", name: "Diaz", age: 38, created_at: Time.now }
 ]
 
-def display_personal_data(personal_data)
-  sorted_data = personal_data.sort_by { |person| -person[:created_at].to_i }
-  sorted_data.each_with_index do |person, index|
-    puts "Person #{index + 1}: #{person[:national_id]} #{person[:name]}, #{person[:age]}"
-  end
-end
+choice = nil
+while choice != 'e'
 
-continue = true
+  clear_terminal
+  display_citizen_data(citizen_data)
 
-while continue
-  display_personal_data(personal_data)
-
+  puts "Enter a letter to select:"
   puts "Add a new person in the database = 'a'"
   puts "Delete a person in the database = 'd'"
+  puts "Search a person in the database = 's'"
   puts "To exit = 'e'"
-  puts "Enter a choice:"
-
-  choice = gets.chomp
+  print "Enter a choice: "
+  choice = gets.chomp.downcase
 
   case choice
-  when "a"
-    puts "Do you want to add a new person to the list? yes = 'y' | no = 'n':"
-    add_new_person = gets.chomp
+  when 'a'
+    puts "Input national id:"
+    new_national_id = gets.chomp
 
-    if add_new_person == "y"
-      puts "Input national id:"
-      new_national_id = gets.chomp
+    puts "Input name:"
+    new_name = gets.chomp
 
-      puts "Input name:"
-      new_name = gets.chomp
+    puts "Input age:"
+    new_age = gets.chomp.to_i
 
-      puts "Input age:"
-      new_age = gets.chomp.to_i
-
-      if personal_data.any? { |person| person[:national_id] == new_national_id }
-        puts "Failed to add: National ID already exists."
-      else
-        personal_data << { national_id: new_national_id, name: new_name, age: new_age, created_at: Time.now }
-        puts "User added successfully!"
-        display_personal_data(personal_data)
-      end
+    if citizen_data.any? { |person| person[:national_id] == new_national_id }
+      puts "Failed to add: National ID already exists."
     else
-      next
+      citizen_data << { national_id: new_national_id, name: new_name, age: new_age, created_at: Time.now }
+      puts "User added successfully!"
+      display_citizen_data(citizen_data)
     end
-  when "d"
+  when 'd'
     puts "Enter the National ID to delete the user:"
     delete_user = gets.chomp
 
-    if personal_data.reject! { |person| person[:national_id] == delete_user }
+    if citizen_data.reject! { |person| person[:national_id] == delete_user }
       puts "Successfully deleted."
-      display_personal_data(personal_data)
+      display_citizen_data(citizen_data)
     else
       puts "User not found."
     end
 
-  when "e"
-    puts "Are you sure you want to exit (y/n)?"
-    exit_choice = gets.chomp
-    if exit_choice == "y"
+  when 's'
+    puts "Enter the National ID to search the user:"
+    search_user = gets.chomp
+
+    user = citizen_data.detect { |person| person[:national_id] == search_user }
+
+    if user
+      puts "User found!"
+      puts "National ID:#{user[:national_id]} Name:#{user[:name]} Age:#{user[:age]}"
+    else
+      puts "User not found."
+    end
+
+  when 'e'
+    print "Are you sure you want to exit (y/n)? "
+    exit_choice = gets.chomp.downcase
+    if exit_choice == 'y'
       puts "Exiting..."
       break
-    else 
-      puts "Going back to the main."
-      next
+    elsif exit_choice == 'n'
+      puts "Going back to the main menu."
+    else
+      puts "Invalid choice. Please enter 'y' or 'n'."
     end
+
   else
-    puts "Invalid choice."
+    puts "Invalid choice. Please enter 'a', 'd', or 'e'."
   end
 end
+
+
+
+
+
