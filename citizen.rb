a2af3d372da9fba1be739edcbb543c61334c79b9
Jaushi <144474840+Jaushi@users.noreply.github.com>
2024-09-17 23:53:51 +0800

    Modified: Added a search feature to find the National ID in the Class

--- citizen.rb
+++ citizen.rb
@@ -26,32 +26,35 @@ class Citizen
   end
 
   def display_record
-    puts "National ID: #{@national_id}, Name: #{@name}, Age: #{@age}, "
+    puts "National ID: #{@national_id}, Name: #{@name}, Age: #{@age}"
   end
 
+  def self.find_by_national_id(national_id)
+    @@records.detect { |person| person.national_id == national_id }
+  end
 end
 
 citizen_data = [
-  { national_id: "1234-5678-9012", name: "Santos", age: 19, created_at: Time.now },
-  { national_id: "3456-7890-1234", name: "Reyes", age: 18, created_at: Time.now },
-  { national_id: "5678-9012-3456", name: "Cruz", age: 21, created_at: Time.now },
-  { national_id: "7890-1234-5678", name: "Bautista", age: 43, created_at: Time.now },
-  { national_id: "9012-3456-7890", name: "Ocampo", age: 32, created_at: Time.now },
-  { national_id: "2345-6789-0123", name: "Garcia", age: 54, created_at: Time.now },
-  { national_id: "4567-8901-2345", name: "Mendoza", age: 52, created_at: Time.now },
-  { national_id: "6789-0123-4567", name: "Torres", age: 36, created_at: Time.now },
-  { national_id: "8901-2345-6789", name: "Flores", age: 48, created_at: Time.now },
-  { national_id: "0123-4567-8901", name: "Ramos", age: 41, created_at: Time.now },
-  { national_id: "2345-6789-0124", name: "Aquino", age: 22, created_at: Time.now },
-  { national_id: "4567-8901-2346", name: "Castillo", age: 29, created_at: Time.now },
-  { national_id: "6789-0123-4568", name: "Rivera", age: 25, created_at: Time.now },
-  { national_id: "8901-2345-6790", name: "Gonzales", age: 37, created_at: Time.now },
-  { national_id: "0123-4567-8902", name: "Dela Cruz", age: 66, created_at: Time.now },
-  { national_id: "2345-6789-0125", name: "Martinez", age: 49, created_at: Time.now },
-  { national_id: "4567-8901-2347", name: "Hernandez", age: 26, created_at: Time.now },
-  { national_id: "6789-0123-4569", name: "Lopez", age: 17, created_at: Time.now },
-  { national_id: "8901-2345-6791", name: "Fernandez", age: 51, created_at: Time.now },
-  { national_id: "4264-4451-5545", name: "Diaz", age: 38, created_at: Time.now }
+  { national_id: "1234-5678-9012", name: "Santos", age: 19 },
+  { national_id: "3456-7890-1234", name: "Reyes", age: 18 },
+  { national_id: "5678-9012-3456", name: "Cruz", age: 21 },
+  { national_id: "7890-1234-5678", name: "Bautista", age: 43 },
+  { national_id: "9012-3456-7890", name: "Ocampo", age: 32 },
+  { national_id: "2345-6789-0123", name: "Garcia", age: 54 },
+  { national_id: "4567-8901-2345", name: "Mendoza", age: 52 },
+  { national_id: "6789-0123-4567", name: "Torres", age: 36 },
+  { national_id: "8901-2345-6789", name: "Flores", age: 48 },
+  { national_id: "0123-4567-8901", name: "Ramos", age: 41 },
+  { national_id: "2345-6789-0124", name: "Aquino", age: 22 },
+  { national_id: "4567-8901-2346", name: "Castillo", age: 29 },
+  { national_id: "6789-0123-4568", name: "Rivera", age: 25 },
+  { national_id: "8901-2345-6790", name: "Gonzales", age: 37 },
+  { national_id: "0123-4567-8902", name: "Dela Cruz", age: 66 },
+  { national_id: "2345-6789-0125", name: "Martinez", age: 49 },
+  { national_id: "4567-8901-2347", name: "Hernandez", age: 26 },
+  { national_id: "6789-0123-4569", name: "Lopez", age: 17 },
+  { national_id: "8901-2345-6791", name: "Fernandez", age: 51 },
+  { national_id: "4264-4451-5545", name: "Diaz", age: 38 }
 ]
 
 citizen_data.each do |person|
@@ -60,7 +63,19 @@ citizen_data.each do |person|
 end
 
 puts Citizen.all_records.inspect
+puts "==========================="
 puts "First person in the Record:"
 Citizen.first_record.display_record
+
 puts "Last person in the Record:"
-puts Citizen.last_record.display_record
+Citizen.last_record.display_record
+
+puts "Enter the National ID to search the user:"
+search_user = gets.chomp
+user = Citizen.find_by_national_id(search_user)
+if user
+  puts "User found!"
+  puts "National ID: #{user.national_id}, Name: #{user.name}, Age: #{user.age}"
+else
+  puts "User not found."
+end
